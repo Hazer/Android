07e53ba3b7f716a3deb79899d7261f5d506aa1e3
Eldin Zenderink <s1077709@student.windesheim.nl>
2018-08-30 20:24:03 +0200

    Build Test

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,53 +1,117 @@
 ﻿using System;
+using Android;
 using Android.App;
+using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
+using Android.Support.V4.App;
 using Android.Support.V7.App;
 using Android.Views;
+using Android.Webkit;
 using Android.Widget;
 
 namespace LittleWeebAndroid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
     public class MainActivity : AppCompatActivity
     {
+        private View rootLayout;
+        private WebView webView;
+        private LittleWeebLibrary.LittleWeeb LittleWeeb = null;
+        private int permissionLevel = 0;
+
+        public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            if (requestCode == 1)
+            {
+
+
+                if (Android.Support.V4.Content.ContextCompat.CheckSelfPermission(this, Manifest.Permission.ReadExternalStorage) != (int)Permission.Granted && Android.Support.V4.Content.ContextCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted)
+                {
+                    // Permission has never been accepted
+                    // So, I ask the user for permission
+                    ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage, Manifest.Permission.ReadExternalStorage }, 1);
+                }
+                else
+                {
+
+
+                    LittleWeeb = new LittleWeebLibrary.LittleWeeb();
+
+                    webView.LoadUrl("file:///android_asset/index.html");
+                    // Permission has already been accepted previously
+                }
+
+            }
+            else
+            {
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            RequestWindowFeature(WindowFeatures.NoTitle);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.Settings.JavaScriptEnabled = true;
+            webView.Settings.CacheMode = CacheModes.Normal;
+            webView.Settings.DatabaseEnabled = true;
+            webView.Settings.DomStorageEnabled = true;
+            webView.Settings.AllowFileAccess = true;
+            webView.Settings.AllowFileAccessFromFileURLs = true;
+            webView.Settings.AllowUniversalAccessFromFileURLs = true;
+            webView.Settings.SetRenderPriority(WebSettings.RenderPriority.High);
+            webView.Settings.SetSupportZoom(true);
+            webView.Settings.UseWideViewPort = true;
+            webView.Settings.SetEnableSmoothTransition(true);
+            webView.Settings.SetLayoutAlgorithm(WebSettings.LayoutAlgorithm.NarrowColumns);
+            webView.SetLayerType(LayerType.Hardware, null);
+            webView.SetWebChromeClient(new MyWebChromeClient());
+            webView.SetWebViewClient(new MyWebViewClient(this));
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            if (Android.Support.V4.Content.ContextCompat.CheckSelfPermission(this, Manifest.Permission.ReadExternalStorage) != (int)Permission.Granted && Android.Support.V4.Content.ContextCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted)
             {
-                return true;
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.WriteExternalStorage, Manifest.Permission.ReadExternalStorage }, 1);
+            }
+            else
+            {
+                LittleWeeb = new LittleWeebLibrary.LittleWeeb();
+
+                webView.LoadUrl("file:///android_asset/index.html");
             }
 
-            return base.OnOptionsItemSelected(item);
         }
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
+        private class MyWebViewClient : WebViewClient
         {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            public Activity mActivity;
+            public MyWebViewClient(Activity activity)
+            {
+                mActivity = activity;
+            }
+
+            public override void OnPageFinished(WebView view, string url)
+            {
+                base.OnPageFinished(view, url);
+
+                var icon = mActivity.FindViewById<RelativeLayout>(Resource.Id.imgIcon);
+
+                icon.Visibility = ViewStates.Gone;
+            }
         }
-	}
+
+        private class MyWebChromeClient : WebChromeClient
+        {
+            public override void OnConsoleMessage(string message, int lineNumber, string sourceID)
+            {
+                base.OnConsoleMessage(message, lineNumber, sourceID);
+            }
+        }
+
+    }
 }
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LittleWeebAndroid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LittleWeeb")]
+[assembly: AssemblyDescription("LittleWeeb is an IRC XDCC based client that uses an easy to use interface where the user no longer has to use commands to download their anime. Simply click on the anime that you want, select the episode, resolution (and possibly source) and your download will start. Even better: You can immediately start watching while your episode is downloading in the background! Have a weeby time, weebs!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LittleWeebAndroid")]
+[assembly: AssemblyCompany("LittleWeeb Dev.")]
+[assembly: AssemblyProduct("LittleWeeb")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.0.*")]
+[assembly: AssemblyFileVersion("0.4.0.*")]
